3b21c199e07789820e3da45ecfadf86569a2e368
vlad <v1vin@yandex.ru>
2021-10-15 17:29:45 +0300

    321

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -3,35 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: oarnoldo <oarnoldo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/14 02:23:41 by marvin            #+#    #+#             */
-/*   Updated: 2021/10/14 02:23:41 by marvin           ###   ########.fr       */
+/*   Updated: 2021/10/15 17:29:28 by oarnoldo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"ft_lib.h"
 
-char *ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-    char *str;
-    int i;
-    int q;
+	char	*str;
+	int		i;
+	int		q;
 
-    i = 0;
-    j = 0;
-    str = malloc(sizeof(char *) * (len + 1));
-    if (!str)
-        return (NULL);
-    while (s[i])
-    {
-        if (s[i]>=start && j < len)
-        {
-            str[j] = s[i];
-            j++;
-        }
-        i++;
-    }
-    str[j] = '\0';
-    return (str);
+	i = 0;
+	q = 0;
+	str = malloc(sizeof(char *) * (len + 1));
+	if (!str)
+		return (NULL);
+	while (s[i])
+	{
+		if (s[i] >= start && q < len)
+		{
+			str[q] = s[i];
+			q++;
+		}
+		i++;
+	}
+	str[q] = '\0';
+	return (str);
 }
