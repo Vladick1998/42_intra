a1f2b4601c0e2af10fb9a660909690833a70aefa
vlad <v1vin@yandex.ru>
2021-10-15 17:41:57 +0300

    123

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oarnoldo <oarnoldo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/15 17:30:48 by oarnoldo          #+#    #+#             */
+/*   Updated: 2021/10/15 17:39:21 by oarnoldo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"ft_lib.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*temp;
+	int		i;
+	int		q;
+
+	i = 0;
+	temp = (char *)malloc((strlen(s1) + strlen(s2) + 1) * sizeof(char));
+	if (temp)
+		return (NULL);
+	while (strlen(s1) > i)
+		temp[i] = s1[i++];
+	q = 0;
+	while (strlen(s2) > q)
+		temp[i++] = s2[q++];
+	temp[i] = '\0';
+	return (temp);
+}
